c2e7834ec255d198b7fd98f3561406dd03decd71
hadi <hadiforooghi1377@gmail.com>
2020-08-31 03:43:30 +0430

    add allowed host

--- aquaMarket_website/settings.py
+++ aquaMarket_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'c54ismx!ntb1pbg9py3k^3yp2)=bmgjie9==o1ku#*+n5+z86v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','https://aquamarket.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0','aquamarket.herokuapp.com','127.0.0.1']
 
 # Application definition
 

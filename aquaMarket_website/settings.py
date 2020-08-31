1c4b09ed236753f5ad2591ebb626819240f8b663
hadi <hadiforooghi1377@gmail.com>
2020-08-31 18:47:24 +0430

    debag True

--- aquaMarket_website/settings.py
+++ aquaMarket_website/settings.py
@@ -24,7 +24,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'c54ismx!ntb1pbg9py3k^3yp2)=bmgjie9==o1ku#*+n5+z86v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0','aquamarket.herokuapp.com','127.0.0.1']
 

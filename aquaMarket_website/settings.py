935565a253c81c39449593fb25cc03f72968e6b3
hadi <hadiforooghi1377@gmail.com>
2020-08-31 18:37:26 +0430

    edit settings.py

--- aquaMarket_website/settings.py
+++ aquaMarket_website/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
